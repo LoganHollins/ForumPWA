c6ad4520718854085692b27e1e830aec31f00c11
LoganHollins <Logan.hollins1@gmail.com>
2017-10-29 20:46:54 -0700

    Use cache when offline only

--- ForumPWA/wwwroot/sw.js
+++ ForumPWA/wwwroot/sw.js
@@ -27,17 +27,14 @@ self.addEventListener('install', function (event) {
 });
 
 self.addEventListener('fetch', function (event) {
-    return new Request('offline.html');
-    return caches.match(new Request('offline.html'));
 
-    return caches.match(event.request)
+    event.respondWith(caches.match(event.request)
         .then(function (cResposne) {
             console.log(navigator.onLine);
             if (!navigator.onLine) {
-                //if (cResposne) {
-                //    return cResposne;
-
-                //}
+                if (cResposne) {
+                    return cResposne;
+                }
 
                 return caches.match(new Request('offline.html'));
             }
@@ -45,46 +42,8 @@ self.addEventListener('fetch', function (event) {
             return fetchAndUpdate(event.request);
         }).catch(function (err) {
             console.error(err);
-        });
-
-    //Not Cache First
-    //event.respondWith(
-    //    caches.match(event.request).then(function (cResposne) {
-    //        if (cResposne)
-    //            fetch(event.request).then(function (fResponse) {
-    //                caches.open(version).then(function (cache) {
-    //                    return cache.put(event.request, fResponse.clone());
-    //                })
-    //            });
-
-    //    })
-    //);
-
-    // Network first
-    //event.respondWith(
-    //    fetch(event.request).then(function (fResponse) {
-    //        return caches.open(version).then(function (cache) {
-    //            if (!fResponse.ok) {
-    //                return cache.match(event.request);
-    //            } else {
-    //                cache.put(event.request, fResponse.clone());
-    //                return fResponse;
-    //            }
-    //        });
-    //    }));
-
-
-    //Fastest (Doesnt work)
-    //event.respondWith(function () {
-    //    var promises = [caches.match(event.request), fetchAndUpdate(event.request)];
-
-    //    return new Promise((resolve, reject) => {
-    //        promises.map(p => Promise.resolve(promise));
-    //        promises.forEach(p => p.then(resolve));
-    //        promises.reduce((a, b) => a.catch(() => b))
-    //            .catch(() => reject(new Error('Both promises failed.')));
-    //    });
-    //});
+        })
+    );
 });
 
 
