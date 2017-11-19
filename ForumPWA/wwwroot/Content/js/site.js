b6236333ad7acb15412ae1e48b993442f54825b6
LoganHollins <Logan.hollins1@gmail.com>
2017-11-19 11:25:49 -0800

    Move to Content folder

---
+++ ForumPWA/wwwroot/Content/js/site.js
@@ -0,0 +1,40 @@
+﻿if ('serviceWorker' in navigator) {
+    navigator
+        .serviceWorker
+        .register('/sw.js', { scope: '/' })
+        .then(console.log)
+        .catch(console.error);
+}
+
+if (Notification.permission !== 'granted' && Notification.permission !== 'denied') {
+    Notification.requestPermission();
+}
+
+function showNotification() {
+    var notification = new Notification('Welcome to the site!', {
+        body: 'Welcome to SuperForum!',
+        badge: '/images/favicon-32x32.png',
+       icon: '/images/android-chrome-512x512.png',
+        tag: 'SuperForumIntro',
+        renotify: true,
+        requireInteraction: false,
+        vibrate: [200, 100, 200],
+        dir: 'ltr',
+        lang: 'en-CA',
+        timestamp: Date.now()
+    });
+
+    notification.addEventListener('error', evt => {
+        console.error('There was a problem', evt);
+    });
+
+    notification.addEventListener('click', evt => {
+        console.log('Notification clicked :)');
+    });
+}
+
+if (!('indexedDB' in window)) {
+    console.log('This browser doesn\'t support IndexedDB');
+}
+
+showNotification();
